b68ced0db1581f910737388580b52f602b7a2eb9
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-10-01 18:45:50 +0200

    Edit: added function doc to filterProperties function

--- amon-react/src/utils/objects.ts
+++ amon-react/src/utils/objects.ts
@@ -1,5 +1,11 @@
 import { BaseObject } from "./interfaces";
 
+/**
+ * Filter properties of a given object based on a list of keys to keep
+ * @param {BaseObject}   base : object to filter
+ * @param {string[]}     keys : list of keys to keep
+ * @returns {BaseObject}      : final object to return
+ */
 const filterProperties = (base: BaseObject, keys: string[]): BaseObject => 
   Object.keys(base)
   .filter((key) => keys.includes(key))
