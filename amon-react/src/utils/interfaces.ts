e352cced562137c1d65535871e489e4a5a1be53c
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-10-05 17:59:54 +0200

    Edit: changed several interfaces into types

--- amon-react/src/utils/interfaces.ts
+++ amon-react/src/utils/interfaces.ts
@@ -1,18 +1,1 @@
-/**
- * Basic Map interface for Type check
- */
- interface BaseMap {
-  [key: string | number]: any
-}
-
-/**
- * Basic Object interface for Type check
- */
-interface BaseObject {
-  [key: string]: any
-}
-
-export type {
-  BaseMap,
-  BaseObject
-}
+export {}
